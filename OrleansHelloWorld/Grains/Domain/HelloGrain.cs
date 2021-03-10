cc7b1c6135327680eafa3b582f5f641d459cbac8
Bogdan Dina <bogdandina@microsoft.com>
2021-03-10 17:42:15 +0200

    working project setup

---
+++ OrleansHelloWorld/Grains/Domain/HelloGrain.cs
@@ -0,0 +1,25 @@
+﻿using GrainInterfaces.Integration;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grains.Domain
+{
+    public class HelloGrain : Orleans.Grain, IHello
+    {
+        private readonly ILogger logger;
+
+        public HelloGrain(ILogger<HelloGrain> logger)
+        {
+            this.logger = logger;
+        }
+
+        public Task<string> SayHello(string greeting)
+        {
+            logger.LogInformation($"\n SayHello message received: greeting = '{greeting}'");
+            return Task.FromResult($"\n Client said: '{greeting}', so HelloGrain says: Hello!");
+        }
+    }
+}
